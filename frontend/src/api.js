268b2cb3514f1ef4d222a15e38388717367512f4
Ayush Jaiswal <ayushjaiswal.madz1303@gmail.com>
2025-07-09 18:15:11 +0530

    Fix: correct AuthRouter import path

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/auth/",
+    baseURL: "https://google-login-io2y.onrender.com/auth/",
     // withCredentials: true,
 });
 
