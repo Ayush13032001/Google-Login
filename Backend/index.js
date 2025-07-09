e2e0e21a37ae83484b1013c094c48a1b52567b08
Ayush Jaiswal <ayushjaiswal.madz1303@gmail.com>
2025-07-09 17:50:48 +0530

    Initial commit with backend and frontend

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+require('./models/dbConnection');
+const PORT = process.env.PORT || 8080;
+const AuthRouter = require('./routes/AuthRouter');
+const cors = require('cors');
+
+
+
+
+app.use(cors())
+app.get('/', (req,res) => { 
+    res.send("hello from auth server")
+})
+
+app.use('/auth', AuthRouter)
+app.listen(PORT , () => {
+    console.log(`Server is running on ${PORT}`);
+
+})
