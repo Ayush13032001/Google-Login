3231fb0379a25ff8d7c8505ca45d03cfdf07bb61
Ayush Jaiswal <105778157+Ayush13032001@users.noreply.github.com>
2026-02-05 14:35:01 +0530

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: "https://google-login-io2y.onrender.com/auth/",
-    // withCredentials: true,
-});
-
-export const googleAuth = (code) => api.get(`/google?code=${code}`);
