b46c2872a1eeed9f16378da8d7830861d75eabe9
Ayush Jaiswal <ayushjaiswal.madz1303@gmail.com>
2026-02-05 16:28:39 +0530

    frontend api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://google-login-2.onrender.com/auth/",
-    // withCredentials: true,
+  baseURL: "https://google-login-2.onrender.com/auth",
 });
 
-export const googleAuth = (code) => api.get(`/google/callback?code=${code}`);
+export const googleAuth = (code) =>
+  api.get(`/google?code=${code}`);
